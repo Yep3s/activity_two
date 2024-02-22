498867894decdc34ac7580dcf903a186ab120c2c
unknown <contactosebastianyepes@gmail.com>
2024-02-22 18:18:26 -0500

    Se agrega la clase conductor aun en proceso de desarrollo

---
+++ Actividad2/src/Conductor.java
@@ -0,0 +1,46 @@
+
+
+/**
+ *
+ * @author Yepes
+ */
+public class Conductor {
+    
+    private String driverName;
+    private int driverIdentification;
+    private int driverAge;
+       
+    public void CrearConductor(String nombre, int cedula, int edad){
+        
+        System.out.println("H");
+        
+    }
+    
+    
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public int getDriverIdentification() {
+        return driverIdentification;
+    }
+
+    public void setDriverIdentification(int driverIdentification) {
+        this.driverIdentification = driverIdentification;
+    }
+
+    public int getDriverAge() {
+        return driverAge;
+    }
+
+    public void setDriverAge(int driverAge) {
+        this.driverAge = driverAge;
+    }
+    
+    
+    
+}
